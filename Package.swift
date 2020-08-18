246da2521cdf3720b11a5045720d2ae1497a9268
Tae Won Ha <h@taewon.de>
2020-08-18 18:17:26 +0200

    Add platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "NvimServerTypes",
+  platforms: [.macOS(.v10_13)],
   products: [.library(name: "NvimServerTypes", targets: ["NvimServerTypes"])],
   dependencies: [],
   targets: [
