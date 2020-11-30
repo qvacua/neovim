b1c7eda8a227a96d6a3be5fcff5ffc4ce4996196
Tae Won Ha <h@taewon.de>
2020-11-30 18:01:08 +0100

    Make build_deps target dependent
    - If we build for different arch than the building machine, shared libs (dylib) are not built which we need to compile libnvim with gettext support

--- NvimServer/bin/build_deps.sh
+++ NvimServer/bin/build_deps.sh
@@ -3,6 +3,7 @@ set -Eeuo pipefail
 
 readonly gettext_version="0.20.1"
 readonly gettext_default_cflags="-g -O2"
+readonly target=${target:?"arm64 or x86_64: you can only build the same target as your machine"}
 
 download_gettext() {
   local -r working_directory=$1
@@ -16,7 +17,6 @@ download_gettext() {
 
 extract_gettext() {
   local -r working_directory=$1
-  local -r target=$2
 
   pushd "${working_directory}" >/dev/null
     tar xf gettext.tar.xz
@@ -29,8 +29,6 @@ build_gettext() {
   local -r cflags=$2
   local -r working_directory=$3
   local -r install_path=$4
-  local -r target=$5
-  local -r host=$6
 
   echo "### Building gettext"
   pushd "${working_directory}/gettext-${target}" >/dev/null
@@ -39,7 +37,6 @@ build_gettext() {
         MACOSX_DEPLOYMENT_TARGET="${deployment_target}" \
         CFLAGS="${cflags} --target=${target}-apple-macos${deployment_target}" \
         CXXFLAGS="${cflags} --target=${target}-apple-macos${deployment_target}" \
-        --host="${host}" \
         --disable-dependency-tracking \
         --disable-silent-rules \
         --disable-debug \
@@ -59,18 +56,21 @@ build_gettext() {
   echo "### Built gettext"
 }
 
-install_universal_lib() {
-  local -r x86_64_install_path_lib=$1
-  local -r arm64_install_path_lib=$2
-  local -r install_path_lib=$3
+package() {
+  local -r install_path=$1
 
-  pushd "${install_path_lib}" >/dev/null
-    lipo -create -output libintl.8.dylib "${x86_64_install_path_lib}/libintl.8.dylib" "${arm64_install_path_lib}.libintl.8.dylib"
-    lipo -create -output libintl.a "${x86_64_install_path_lib}/libintl.a" "${arm64_install_path_lib}/libintl.a"
+  pushd "${install_path}" >/dev/null
+    tar cjf "gettext-${target}.tar.bz2" include lib
+    echo "Packaged gettext for ${target} at $(realpath "gettext-${target}.tar.bz2")"
   popd >/dev/null
 }
 
 main() {
+  if [[ "$(uname -m)" != "${target}" ]]; then
+    echo "We are not on ${target}!"
+    exit 1
+  fi
+
   echo "### Building deps"
   pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
 
@@ -82,40 +82,25 @@ main() {
   rm -rf "${install_path_lib}" && mkdir -p "${install_path_lib}"
   rm -rf "${install_path_include}" && mkdir -p "${install_path_include}"
 
-  local -r x86_64_install_path="${install_path}-x86_64"
-  local -r arm64_install_path="${install_path}-arm64"
-  rm -rf "${x86_64_install_path}" && mkdir -p "${x86_64_install_path}"
-  rm -rf "${arm64_install_path}" && mkdir -p "${arm64_install_path}"
+  local -r target_install_path="${install_path}-${target}"
+  rm -rf "${target_install_path}" && mkdir -p "${target_install_path}"
 
   local -r working_directory="$(realpath ./third-party/.deps)"
   mkdir -p "${working_directory}"
   download_gettext "${working_directory}"
 
-  local target="arm64"
   local deployment_target
   deployment_target=$(cat "./Resources/${target}_deployment_target.txt")
-  extract_gettext "${working_directory}" "${target}"
+  readonly deployment_target
+
+  extract_gettext "${working_directory}"
   build_gettext \
       "${deployment_target}" \
       "${gettext_default_cflags}" \
       "${working_directory}" \
-      "${arm64_install_path}" \
-      "${target}" \
-      "arm-apple-macos"
+      "${target_install_path}"
 
-  target="x86_64"
-  deployment_target=$(cat "./Resources/${target}_deployment_target.txt")
-  extract_gettext "${working_directory}" "${target}"
-  build_gettext \
-      "${deployment_target}" \
-      "${gettext_default_cflags}" \
-      "${working_directory}" \
-      "${x86_64_install_path}" \
-      "${target}" \
-      "x86_64-apple-macos"
-
-  install_universal_lib "${x86_64_install_path}/lib" "${arm64_install_path}/lib" "${install_path_lib}"
-  cp -r "${arm64_install_path}/include"/* "${install_path_include}"
+  package "${target_install_path}"
 
   popd >/dev/null
   echo "### Built deps"
